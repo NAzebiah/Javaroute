ee61efedfc4b2d976aef136436740ddfabfd9216
Prabhu Kumanan <Prabhu.Kumanan@scouts.org.uk>
2018-12-06 11:07:23 +0000

    test project

---
+++ src/main/java/com/demo/order/Input.java
@@ -0,0 +1,33 @@
+package com.demo.order;
+
+public class Input {
+private String fName;
+private String lName;
+private String age;
+private String country;
+public String getfName() {
+ return fName;
+}
+public void setfName(String fName) {
+ this.fName = fName;
+}
+public String getlName() {
+ return lName;
+}
+public void setlName(String lName) {
+ this.lName = lName;
+}
+public String getAge() {
+ return age;
+}
+public void setAge(String age) {
+ this.age = age;
+}
+public String getCountry() {
+ return country;
+}
+public void setCountry(String country) {
+ this.country = country;
+}
+
+}
---
+++ src/main/java/com/demo/order/OrderProcessor.java
@@ -0,0 +1,17 @@
+package com.demo.order;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+
+public class OrderProcessor  implements Processor {
+
+ @Override
+ public void process(Exchange exchange) throws Exception {
+  Input input = exchange.getIn().getBody(Input.class);
+  Output output=new Output();
+  output.setName(input.getfName()+" "+input.getlName());
+  output.setStatus("OK");
+  exchange.getOut().setBody(output);
+ }
+
+}
